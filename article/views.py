f05e6b740ccd5356754f53b159319603a7270fe9
Vivian Lin <vivian.wj.lin@gmail.com>
2023-07-16 23:39:08 +0800

    api/articles/id (PATCH) setup

--- article/views.py
+++ article/views.py
@@ -11,7 +11,7 @@ from .serializers import ArticleSerializer
 
 
 @csrf_exempt
-@api_view(['GET', 'POST', 'DELETE'])
+@api_view(['GET', 'POST', 'DELETE', 'PATCH'])
 def manage_articles(request, article_id=None):
     if request.method == 'GET':
         if article_id:
@@ -30,7 +30,7 @@ def manage_articles(request, article_id=None):
         serializer = ArticleSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response({'message': '文章已成功建立'})
+            return Response({'message': '文章已成功建立'}, status=status.HTTP_200_OK)
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
@@ -39,8 +39,24 @@ def manage_articles(request, article_id=None):
             try:
                 article = Article.objects.get(id=article_id)
                 article.delete()
-                return Response({'message': '文章已成功刪除'})
+                return Response({'message': '文章已成功刪除'}, status=status.HTTP_200_OK)
             except Article.DoesNotExist:
                 return Response({'error': '找不到指定的文章'}, status=status.HTTP_404_NOT_FOUND)
 
-    return JsonResponse({'error': '請求方法不支援'})
+    elif request.method == 'PATCH':
+        if article_id:
+            try:
+                article = Article.objects.get(id=article_id)
+                serializer = ArticleSerializer(
+                    article, data=request.data, partial=True)
+                if serializer.is_valid():
+                    serializer.save()
+                    return Response({'message': '文章已成功修改'}, status=status.HTTP_200_OK)
+                else:
+                    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            except Article.DoesNotExist:
+                return Response({'error': '找不到指定的文章'}, status=status.HTTP_404_NOT_FOUND)
+        else:
+            return Response({'error': '缺少文章ID'}, status=status.HTTP_400_BAD_REQUEST)
+
+    return JsonResponse({'error': '其他錯誤'}, status=500)
