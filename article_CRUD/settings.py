b96536f179d00c149760246ba38d8437b748e426
Vivian Lin <vivian.wj.lin@gmail.com>
2023-07-26 09:30:43 +0800

    JWT settings updated

--- article_CRUD/settings.py
+++ article_CRUD/settings.py
@@ -146,9 +146,9 @@ REST_FRAMEWORK = {
 
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=30),
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=1),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
-    'ROTATE_REFRESH_TOKENS': False,
+    'ROTATE_REFRESH_TOKENS': True,
 }
 
 LOGGING = {
